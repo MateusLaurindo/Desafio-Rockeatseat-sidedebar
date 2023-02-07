fa9c6a5e3cf257018324604e1455e32f83762bfa
Mateus Lima <102548678+MateusLaurindo@users.noreply.github.com>
2023-02-07 19:53:05 +0000

    Atualização

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function clicou() {
-  const teste = document.querySelector('nav');
+  const menu = document.querySelector('nav');
+  const botao = document.querySelector('button');
+  const exit = document.querySelector('.info-saida');
+  const logo = document.querySelector('.logo');
+  const saida = document.querySelector('.saida');
+  const texto1 = document.querySelector('.texto1');
 
-  teste.classList.toggle('trocar')
+  exit.classList.toggle('info-saida-mover');
+  botao.classList.toggle('botao-mover');
+  menu.classList.toggle('trocar');
+  logo.classList.toggle('logo-real');
+  saida.classList.toggle('saida-ativo');
+  texto1.classList.toggle('texto-aparecer');
 }
+
